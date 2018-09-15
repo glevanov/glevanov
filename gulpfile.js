d9a3b9db41f6430155562419b8e129f53cbf32b3
Grigoriy Levanov <glevanov@gmail.com>
2018-09-15 20:47:41 +0300

    Adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create()
+const csso = require('gulp-csso')
+
+gulp.task('styles', function () {
+  return gulp.src('./src/style.css')
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+})
+gulp.task('dev', ['browserSync'], function () {
+  gulp.watch('./src/style.css', ['styles'])
+  gulp.watch('./*.html', browserSync.reload)
+})
