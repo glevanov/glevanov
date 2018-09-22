32155ca43c3005779723128d717c6bc075c3c00c
Grigoriy Levanov <glevanov@gmail.com>
2018-09-22 13:23:31 +0300

    Adds Prettier, upgrades portfolio cards

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
-const gulp = require('gulp')
-const autoprefixer = require('gulp-autoprefixer')
-const browserSync = require('browser-sync').create()
-const csso = require('gulp-csso')
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const csso = require("gulp-csso");
 
-gulp.task('styles', function () {
-  return gulp.src('./src/style.css')
+gulp.task("styles", function() {
+  return gulp
+    .src("./src/style.css")
     .pipe(autoprefixer())
     .pipe(csso())
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-})
-gulp.task('browserSync', function () {
+    .pipe(gulp.dest("./"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+gulp.task("browserSync", function() {
   browserSync.init({
     server: {
-      baseDir: './'
-    },
-  })
-})
-gulp.task('dev', ['browserSync'], function () {
-  gulp.watch('./src/style.css', ['styles'])
-  gulp.watch('./*.html', browserSync.reload)
-})
+      baseDir: "./"
+    }
+  });
+});
+gulp.task("dev", ["browserSync"], function() {
+  gulp.watch("./src/style.css", ["styles"]);
+  gulp.watch("./*.html", browserSync.reload);
+});
